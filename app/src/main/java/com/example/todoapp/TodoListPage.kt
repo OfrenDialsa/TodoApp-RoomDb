cb77afc98ff5a8da29e829cb7ee5a3cbcb28e021
Nero24 <lolilolipo24@gmail.com>
2024-09-06 19:28:29 +0700

    Upload

---
+++ app/src/main/java/com/example/todoapp/TodoListPage.kt
@@ -0,0 +1,11 @@
+package com.example.todoapp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TodoListPage(modifier: Modifier = Modifier) {
+    val todoList = getFakeTodo()
+    Text(text = todoList.toString())
+}
